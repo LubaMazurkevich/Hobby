82f8d2dffc79be5f1ee5bc67200aeef254ef2d5a
Luba <lubou.mazurkevich@gmail.com>
2021-09-21 19:58:01 +0300

    1)made possibility to add book by exact user 2)correct view of templates

--- Hobby/library/models.py
+++ Hobby/library/models.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 class Book(models.Model):
-    # user=models.ManyToManyField(settings.AUTH_USER_MODEL)
-    author = models.ManyToManyField(User) #НОВАЯ СТРОКА
+    author = models.ManyToManyField(User)
     title = models.CharField(max_length=100)
     description = models.TextField()
     genre = models.CharField(max_length=20)
     image = models.ImageField(null=True,blank=True,upload_to='media/img/')
+
--- Hobby/library/views.py
+++ Hobby/library/views.py
@@ -7,17 +7,18 @@ from .forms import BookForm
 # Create your views here.
 from .models import Book
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def book_index(request):
-    books=Book.objects.all() #выполняем запрос,он позволяет создавать,извлекать объекты в базе данных
-    #запрос возвращает коллекцию всех объектов,он вернет коллекцию всех книг
+    books=Book.objects.all()
+    #запрос  вернет коллекцию всех книг
     context = {
         'books': books
     }
     return render(request, 'book_index.html', context)
 
 def book_detail(request,pk):
-    book=Book.objects.get(pk=pk) #делаем запрос,запрос извлекает проект с первичным ключом pk(ключ определенного проекта)
+    book=Book.objects.get(pk=pk) # извлекает проект с первичным ключом pk(ключ определенного проекта)
     context={
         'book': book
     }
@@ -27,24 +28,24 @@ def book_detail(request,pk):
 def books_new(request):
     if request.method == "POST":
         form = BookForm(request.POST, request.FILES) #POST-это публикация данных,все поля формы теперь в request.POST
-        print(request.FILES)
-        for i in form.data.items():
-            print(i)
         if form.is_valid():
-            # print("ter [ashete3")
             book = form.save(commit=False)
             book.save()
+            book.author.add(User.objects.get_by_natural_key(request.user))
             return redirect('book_detail', pk=book.pk) # переадресация на страницу после добавления новой записи
     else:
-        form = BookForm()   #это когда хотим получить пустую форму??
+        form = BookForm()
     return render(request, 'books.html', {'form': form})
 
 
+
 def my_books(request):
-    my_books = Book.objects.filter(author=request.user)
-    context = {
-        'my_books': my_books
-    }
+    if request.user.is_authenticated:
+        my_books = Book.objects.filter(author=request.user)
+        context = {
+            'my_books': my_books
+        }
 
-    return render(request, 'my_books.html', context)
-
+        return render(request, 'my_books.html', context)
+    else:
+        return HttpResponse('No authenticated,please login and then add books')
