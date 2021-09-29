059e86763d3975dbd9693bdf6ad821d1f1194b13
Luba <lubou.mazurkevich@gmail.com>
2021-09-29 12:35:07 +0300

    added serials to main page

--- Hobby/Hobby/urls.py
+++ Hobby/Hobby/urls.py
@@ -17,15 +17,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('library.urls')), #страница называлась books
+    path('library', include('library.urls')),
     path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
     #Add Django site authentication urls (for login, logout, password management)
     path('accounts/', include('accounts.urls')),
+    path('films/', include('films.urls'), name='films'),
+    path('serials/', include('serials.urls'), name='serials'),
+    path('', views.book_film, name='home')
 ]
 
 
--- Hobby/Hobby/views.py
+++ Hobby/Hobby/views.py
@@ -2,11 +2,13 @@
 from django.shortcuts import render
 from films.models import Film
 from library.models import Book
+from serials.models import Tvserial
 
 
 def book_film(request):
     books = Book.objects.all()
     films = Film.objects.all()
-    context = {'books': books, 'films': films}
+    serials = Tvserial.objects.all()
+    context = {'books': books, 'films': films, 'serials': serials}
 
     return render(request, 'home.html',context)
