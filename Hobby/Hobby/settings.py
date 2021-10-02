c39dbb0a581217f89910b13e3b3b1197690e6453
Luba <lubou.mazurkevich@gmail.com>
2021-10-02 16:03:25 +0300

    change database from sqllite3 to postgresql

--- Hobby/Hobby/settings.py
+++ Hobby/Hobby/settings.py
@@ -82,13 +82,17 @@ WSGI_APPLICATION = 'Hobby.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Hobby_DB',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
